c85eccabe8ef9d7440bd708802d3fc68330150f7
Adrien Bourgain <adn@MacBook-Pro-de-Adrien.local>
2022-08-01 11:43:44 +0200

    step-5

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
 
+import 'constants/routes.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -19,9 +20,9 @@ void main() {
         ),
         home: const HomePage(),
         routes: {
-          '/login/': (context) => const LoginView(),
-          '/register/': (context) => const RegisterView(),
-          '/notes/': (context) => const NotesView(),
+          loginRoute: (context) => const LoginView(),
+          registerRoute: (context) => const RegisterView(),
+          notesRoute: (context) => const NotesView(),
         }),
   );
 }
@@ -80,7 +81,7 @@ class _NotesViewState extends State<NotesView> {
                 if (shouldLogout) {
                   await FirebaseAuth.instance.signOut();
                   Navigator.of(context)
-                      .pushNamedAndRemoveUntil('/login/', (_) => false);
+                      .pushNamedAndRemoveUntil(loginRoute, (_) => false);
                 }
             }
           },
