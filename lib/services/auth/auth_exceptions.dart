afc3c1228a121ec8b62ae20acf9c0a218e04f474
Adrien Bourgain <adn@MacBook-Pro-de-Adrien.local>
2022-08-02 22:39:36 +0200

    step-8

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+// login exceptions
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+// register exceptions
+class InvalidEmailAuthException implements Exception {}
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+// generic exceptions
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
